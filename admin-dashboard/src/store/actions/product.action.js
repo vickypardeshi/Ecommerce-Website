474970a5ef851b91fd181330cef5b819356020e4
Vicky Pardeshi <vickypardesh@gmail.com>
2021-04-09 02:07:47 +0530

    Fix : add product issue

--- admin-dashboard/src/store/actions/product.action.js
+++ admin-dashboard/src/store/actions/product.action.js
@@ -1,8 +1,30 @@
 import axios from '../../api/axios';
+import { productConstants } from './constants';
 
 export const addProduct = (form) => {
     return async dispatch => {
+
+        dispatch({
+            type: productConstants.ADD_NEW_PRODUCT_REQUEST,
+        });
+
         const res = await axios.post('/product/create', form);
-        console.log(res);
+        if(res.status === 201){
+            dispatch({
+                type: productConstants.ADD_NEW_PRODUCT_SUCCESS,
+                payload: {
+                    products: res.data.product,
+                }
+            });
+        }
+        else{
+            dispatch({
+                type: productConstants.ADD_NEW_PRODUCT_FAILURE,
+                payload: {
+                    message: 'Something went wrong'
+                }
+            });
+        }
+        //console.log(res);
     }
 }
--- admin-dashboard/src/store/reducers/product.reducer.js
+++ admin-dashboard/src/store/reducers/product.reducer.js
@@ -2,6 +2,8 @@ import { productConstants } from "../actions/constants";
 
 const initState = {
     products: [],
+    loading: false,
+    error: '',
 }
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initState, action) => {
@@ -10,6 +12,30 @@ export default (state = initState, action) => {
             state = {
                 ...state,
                 products: action.payload.products,
+            }
+            break;
+        case productConstants.ADD_NEW_PRODUCT_REQUEST:
+            state = {
+                ...state,
+                loading: true,
+            }
+            break;
+        case productConstants.ADD_NEW_PRODUCT_SUCCESS:
+            const product = action.payload.products;
+            const updatedProducts = state.products.push({
+                ...product
+            });
+            console.log(updatedProducts);
+            state = {
+                ...state,
+                loading: false,
+            }
+            break;
+        case productConstants.ADD_NEW_PRODUCT_FAILURE:
+            state = {
+                ...state,
+                loading: false,
+                error: action.payload.error,
             }
             break;
         default:
