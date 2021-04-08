eb818e439554e1205d6c2f863c46c06576dfee7e
Vicky Pardeshi <vickypardesh@gmail.com>
2021-04-08 15:51:30 +0530

    Create initial data request API

---
+++ ecommercebackend/src/controller/admin/initialData.js
@@ -0,0 +1,39 @@
+const Category = require('../../models/category');
+const Product = require('../../models/product');
+
+function getCategoriesList(categories, parentId=null){
+    const categoryList = [];
+    let category;
+
+    if(parentId === null){
+        category = categories.filter(cat => cat.parentId == undefined);
+    }
+    else{
+        category = categories.filter(cat => cat.parentId == parentId);
+    }
+    for(let cat of category){
+        categoryList.push({
+            _id: cat._id,
+            name: cat.name,
+            slug: cat.slug,
+            parentId: cat.parentId,
+            children: getCategoriesList(categories, cat._id), //call for getting child category
+        });
+    }
+
+    return categoryList;
+}
+
+exports.initialData = async (req, res) => {
+
+    const categories = await Category.find({}).exec();
+    const products = await Product.find({})
+        .select('_id name price quantity slug description productPictures category')
+        .exec();  //channing used
+        //.populate('category') //belong to diff collection & it will fill the relavent data
+
+    res.status(200).json({
+        categories: getCategoriesList(categories),
+        products
+    });
+}
--- ecommercebackend/src/index.js
+++ ecommercebackend/src/index.js
@@ -11,6 +11,7 @@ const adminRoutes = require('./routes/admin/auth');
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
+const initialDataRoutes = require('./routes/admin/initialData');
 
 //environment variable
 env.config();
@@ -38,6 +39,7 @@ app.use('/api', adminRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', productRoutes);
 app.use('/api', cartRoutes);
+app.use('/api', initialDataRoutes);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
